9e934f068a4c6f2993587d18628fe1cf606785b1
Martin Skachkov <mgsemailbox@gmail.com>
2024-01-04 18:23:34 +0200

    add

--- graphs.pl
+++ graphs.pl
@@ -15,6 +15,10 @@ edge(G,V,W) :- elem([V|X],G), elem(W,X).
 % Условие: G е напълно краен граф.
 vertices_c(G,VV) :- findall(V, vertex(G,V), X), sort(X,VV). 
 
+% ребра(G,VV) - EE e списък от ребрата в G.
+% Условие: G е напълно краен граф.
+edges(G,EE) :- findall((V,W), edge(G,V,W), X), sort(X,EE).
+
 % път_от_до(G,X,V,W) - X e път в G, започващ от V и завършващ в W
 % Условие: Известно е ограничение отгоре за дължината на Х или G e ацикличен краен граф.
 path_from_to(G,[V],V,V) :- vertex(G,V).	% with one vertex
@@ -39,13 +43,31 @@ part([_|V],[B|Col],I,X) :- B #\= I, part(V,Col,I,X).
 generate_partition(G,A,S,B) :- vertices_c(G,VV), len(VV,N), len(Colors,N), Colors ins 0..2, label(Colors), %оцветяваме всеки връх
     						   part(VV,Colors,0,A),
 							   part(VV,Colors,1,S),
-    						   part(VV,Colors,2,B),
+    						   part(VV,Colors,2,B).
+
+% separate(VV,EE,Colors) - за всеки елемент (V1,V2) на ЕЕ, където V1=VV[k1] V2=VV[k2], e вярно
+%						   Colors[k1]=Colors[k2] или Colors[k1]=0 или Colors[k2]=0
+separate(VV,[(V1,V2)|EE],Colors) :- C1 #= C2 #\/ C1 #= 0 #\/ C2 #= 0,
+    								nth(VV,K1,V1), nth(VV,K2,V2),
+    								nth(Colors,K1,C1), nth(Colors,K2,C2),
+    								separate(VV,EE,Colors).
+
+generate_separators(G,A,S,B) :- vertices_c(G,VV), len(VV,N), len(Colors,N), Colors ins 0..2,
+    							edges(G,EE), % намираме ребрата
+    							separate(VV,EE,Colors), % този предикат казва че не искаме да има ребра между А и В
+								label(Colors), % намери такова разпределение на цветовете
+    							part(VV,Colors,1,A),
+							    part(VV,Colors,0,S),
+    						    part(VV,Colors,2,B).
 % Helpful funcs
 elem(A,[A|_]).
 elem(A,[_|X]) :- elem(A,X).
 
 len([],0).
 len([_|X],N) :- N #> 0, N #= N1 + 1, len(X,N1).
+
+nth([A|_],N,A) :- N #= 1.
+nth([_|X],N,A) :- N #> 0, N #= N1 + 1, nth(X,N1,A).
 
 :- discontiguous vertex/2.
 :- discontiguous edge/3.
